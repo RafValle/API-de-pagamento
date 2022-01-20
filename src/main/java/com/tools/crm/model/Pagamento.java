4f8b765d459040efbe4a0c170572db7c8b73f2d5
Rafael Valle <rafael_valle25@hotmail.com>
2022-01-19 23:32:36 -0300

    testes

--- src/main/java/com/tools/crm/model/Pagamento.java
+++ src/main/java/com/tools/crm/model/Pagamento.java
@@ -17,7 +17,6 @@ public class Pagamento {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-//	private Long idPagamento;
 	private String cartao;
 	private Double valor;
 	private LocalDate dateTime;
